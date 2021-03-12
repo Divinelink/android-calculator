81181a3799469d78f053e2e553a4ec34f9995d37
Haris Andreolas <divinedlink90@yahoo.gr>
2021-03-12 22:05:54 +0200

    a really bad way to store an access_key

--- app/src/main/java/handreolas/divinelink/calculator/rest/RestAPI.java
+++ app/src/main/java/handreolas/divinelink/calculator/rest/RestAPI.java
@@ -9,9 +9,11 @@ import retrofit2.http.GET;
 
 public interface RestAPI {
 
+    @GET("latest?access_key=f71af430fc192cbb2e4ed4045799778d")
     Call<CurrencyRateModel> fetchLatestRates();
 
 
+    @GET("symbols?access_key=f71af430fc192cbb2e4ed4045799778d")
     Call<CurrencySymbolsModel> fetchSymbols();
 
 }
